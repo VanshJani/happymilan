1fffcfd532c8c51dfe13f015b1fbdc9c266e51c2
VanshJani <140586866+VanshJani@users.noreply.github.com>
2024-09-28 21:48:30 +0530

    Update UsersAction.js

--- store/actions/UsersAction.js
+++ store/actions/UsersAction.js
@@ -517,6 +517,7 @@ export const logoutuser = () => {
             deleteCookie('data', { path: '/' });
             deleteCookie('userid', { path: '/' });
             deleteCookie('fcmToken', { path: '/' });
+            deleteCookie('UserProfile', { path: '/' });
             localStorage.clear(); // Clear local storage
 
         } catch (error) {
