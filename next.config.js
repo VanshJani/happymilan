275b00d33c89e005733caa2abdbd6ce2f86a7e98
VanshJani <140586866+VanshJani@users.noreply.github.com>
2024-09-29 11:26:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    disableStaticImages: true,
-    unoptimized: true,
+    disableStaticImages: false, // Enable static image optimization
+    unoptimized: false, // Set to false to optimize images
+    domains: ['https://happymilan.vercel.app/'], // Your domain for external images
+    formats: ['image/avif', 'image/webp'], // Enable AVIF and WebP formats for optimization
   },
 }
 
